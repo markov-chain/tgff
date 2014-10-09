cab356700a8b96836e46ad7978b8b79460783723
Ivan Ukhov <ivan.ukhov@gmail.com>
2014-10-09 22:08:53 +0200

    Set up Cargo

---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+//! The package provides a parser for the
+//! [TGFF](http://ziyang.eecs.umich.edu/~dickrp/tgff/) (Task Graphs For Free)
+//! format, which is a format for storing task graphs and accompanying data
+//! used in scheduling and allocation research.
+
+#[test]
+fn it_works() {
+}
