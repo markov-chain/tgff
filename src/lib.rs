edce8e3d6bb262ea1a9ba23258a3b774081b870c
Ivan Ukhov <ivan.ukhov@gmail.com>
2014-10-12 07:27:30 +0200

    Removed IO; began implementing char-by-char parsing

--- src/lib.rs
+++ src/lib.rs
@@ -3,13 +3,145 @@
 //! format, which is a format for storing task graphs and accompanying data
 //! used in scheduling and allocation research.
 
-use std::io::{File, IoResult};
+#![feature(macro_rules)]
 
-pub fn parse(path: Path) -> IoResult<()> {
-    let mut file = match File::open(&path) {
-        Ok(file) => file,
-        Err(err) => return Err(err),
-    };
+pub type ParserResult<T> = Result<T, ParserError>;
 
-    Ok(())
+pub struct ParserError {
+    line: uint,
+    message: &'static str,
+}
+
+impl std::fmt::Show for ParserError {
+    fn fmt(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(formatter, "{} on line {}", self.message, self.line)
+    }
+}
+
+macro_rules! raise(
+    ($parser:expr, $message:expr) => (
+        return Err(ParserError { line: $parser.line, message: $message });
+    );
+)
+
+pub struct Parser<'a> {
+    data: &'a str,
+    cursor: std::iter::Peekable<(uint, char), std::str::CharOffsets<'a>>,
+    line: uint,
+}
+
+impl<'a> Parser<'a> {
+    pub fn new(data: &'a str) -> Parser<'a> {
+        Parser {
+            data: data,
+            cursor: data.char_indices().peekable(),
+            line: 1,
+        }
+    }
+
+    pub fn process(&mut self) -> ParserResult<()> {
+        return Ok(());
+
+        loop {
+            self.skip_whitespace();
+
+            match self.peek() {
+                Some(&(_, '\n')) => self.skip(),
+                Some(&(_, '@')) => { self.process_at(); },
+                _ => {},
+            }
+        }
+
+        Ok(())
+    }
+
+    #[inline]
+    fn peek(&mut self) -> Option<&(uint, char)> {
+        self.cursor.peek()
+    }
+
+    #[inline]
+    fn skip(&mut self) {
+        self.next();
+    }
+
+    fn skip_whitespace(&mut self) {
+        loop {
+            match self.peek() {
+                Some(&(_, '\t')) |
+                Some(&(_, ' ')) => self.skip(),
+                _ => break,
+            }
+        }
+    }
+
+    fn process_at(&mut self) -> ParserResult<()> {
+        self.skip(); // @
+
+        let mut name = String::new();
+        for (_, c) in self {
+            match c {
+                ' ' | '\t' => break,
+                _ => name.push(c),
+            }
+        }
+
+        if name.is_empty() {
+            raise!(self, "the name of an @-statement should not be empty");
+        }
+
+        self.skip_whitespace();
+
+        let mut number = String::new();
+        for (_, c) in self {
+            match c {
+                ' ' | '\t' => break,
+                _ => name.push(c),
+            }
+        }
+
+        if number.is_empty() {
+            raise!(self, "the number of an @-statement should not be empty");
+        }
+
+        Ok(())
+    }
+}
+
+impl<'a> std::iter::Iterator<(uint, char)> for Parser<'a> {
+    fn next(&mut self) -> Option<(uint, char)> {
+        let pair = self.cursor.next();
+        match pair {
+            Some((_, '\n')) => self.line += 1,
+            _ => {},
+        }
+        pair
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    macro_rules! assert_err(
+        ($e: expr) => (
+            if let Ok(_) = $e {
+                assert!(false, "expected an error");
+            }
+        );
+    )
+
+    #[test]
+    fn skip_whitespace() {
+        let mut parser = super::Parser::new("  \t  abc");
+        parser.skip_whitespace();
+        assert_eq!(parser.next().unwrap(), (5, 'a'));
+    }
+
+    #[test]
+    fn process_at() {
+        let mut parser = super::Parser::new("@ ");
+        assert_err!(parser.process_at());
+
+        parser = super::Parser::new("@abc");
+        assert_err!(parser.process_at());
+    }
 }
--- tests/lib.rs
+++ tests/lib.rs
@@ -1,15 +1,29 @@
+#![feature(macro_rules)]
+
 extern crate tgff;
 extern crate test;
 
 use std::io::fs::PathExtensions;
+use std::io::File;
+use tgff::Parser;
+
+macro_rules! assert_ok(
+    ($e: expr) => (
+        if let Err(err) = $e {
+            assert!(false, "{}", err);
+        }
+    );
+)
 
 #[test]
-fn parse() {
-    assert!(tgff::parse(find_fixture("simple.tgff")).is_ok());
+fn parser_process() {
+    let content = read_fixture("simple.tgff");
+    let mut parser = Parser::new(content.as_slice());
+    assert_ok!(parser.process());
 }
 
-fn find_fixture(name: &'static str) -> Path {
+fn read_fixture(name: &'static str) -> String {
     let path = Path::new("tests").join_many(["fixtures", name]);
     assert!(path.exists());
-    path
+    File::open(&path).read_to_string().unwrap()
 }
