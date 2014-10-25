f555c1ad778f22145333b63d956c8ddab7600276
Ivan Ukhov <ivan.ukhov@gmail.com>
2014-10-25 08:32:12 +0200

    Replaced fixture simple with fixture 002_040

--- benches/lib.rs
+++ benches/lib.rs
@@ -3,8 +3,8 @@ extern crate test;
 extern crate tgff;
 
 #[bench]
-fn parse_simple(b: &mut test::Bencher) {
-    let content = read_fixture("simple.tgff");
+fn parse_002_040(b: &mut test::Bencher) {
+    let content = read_fixture("002_040.tgff");
 
     b.iter(|| {
         test::black_box(tgff::parse(content.as_slice()).unwrap())
@@ -22,7 +22,7 @@ fn parse_032_640(b: &mut test::Bencher) {
 
 fn read_fixture(name: &str) -> String {
     use std::io::fs::PathExtensions;
-    let path = Path::new("fixtures").join(name);
+    let path = Path::new("tests").join_many(["fixtures", name]);
     assert!(path.exists());
     std::io::File::open(&path).read_to_string().unwrap()
 }
--- tests/lib.rs
+++ tests/lib.rs
@@ -6,62 +6,61 @@ extern crate test;
 extern crate tgff;
 
 #[test]
-fn parse_simple() {
-    let r = tgff::parse(read_fixture("simple.tgff").as_slice()).unwrap();
+fn parse_002_040() {
+    let result = tgff::parse(read_fixture("002_040.tgff").as_slice()).unwrap();
 
-    assert_eq!(r.attributes["HYPERPERIOD".to_string()], 1180);
-    assert_eq!(r.graphs.len(), 5);
-    assert_eq!(r.tables.len(), 3);
+    assert_eq!(result.attributes["HYPERPERIOD".to_string()], 8);
+    assert_eq!(result.graphs.len(), 1);
+    assert_eq!(result.tables.len(), 2);
 
-    let periods = vec![590, 1180, 1180, 590, 1180];
-    let tasks = vec![12, 20, 24, 8, 20];
-    let arcs = vec![19, 25, 28, 7, 24];
-    let deadlines = vec![1, 6, 8, 3, 6];
+    let graph = &result.graphs[0];
+    assert_eq!(graph.name, "GRAPH".to_string());
+    assert_eq!(graph.id, 0);
+    assert_eq!(graph.attributes["PERIOD".to_string()], 8);
+    assert_eq!(graph.tasks.len(), 40);
+    assert_eq!(graph.arcs.len(), 52);
+    assert_eq!(graph.deadlines.len(), 18);
 
-    for i in range(0u, 5) {
-        assert_eq!(r.graphs[i].name, "TASK_GRAPH".to_string());
-        assert_eq!(r.graphs[i].id, i);
-        assert_eq!(r.graphs[i].attributes["PERIOD".to_string()], periods[i]);
-        assert_eq!(r.graphs[i].tasks.len(), tasks[i]);
-        assert_eq!(r.graphs[i].arcs.len(), arcs[i]);
-        assert_eq!(r.graphs[i].deadlines.len(), deadlines[i]);
+    let prices = vec![10.5042, 14.8562];
+    for i in range(0u, 2) {
+        let table = &result.tables[i];
+        assert_eq!(table.name, "CORE".to_string());
+        assert_eq!(table.id, i);
+        assert_eq!(table.attributes["price".to_string()] as f32, prices[i]);
+        assert_eq!(table.columns.len(), 4);
+        assert_eq!(table.columns[0].name, "type".to_string());
+        assert_eq!(table.columns[1].name, "version".to_string());
+        assert_eq!(table.columns[2].name, "dynamic_power".to_string());
+        assert_eq!(table.columns[3].name, "execution_time".to_string());
     }
 
-    let prices = vec![70.1121, 71.4235, 80.491];
-
-    for i in range(0u, 3) {
-        assert_eq!(r.tables[i].name, "COMMUN".to_string());
-        assert_eq!(r.tables[i].id, i);
-        assert_eq!(r.tables[i].attributes["price".to_string()] as f32, prices[i]);
-        assert_eq!(r.tables[i].columns.len(), 2);
-        assert_eq!(r.tables[i].columns[0].name, "type".to_string());
-        assert_eq!(r.tables[i].columns[1].name, "exec_time".to_string());
-    }
-
-    let data = vec![
-        48.5893, 33.4384, 34.2468, 51.2027, 51.3571, 30.3827, 43.3982, 60.9097,
-        36.0322, 34.7446, 45.3479, 31.7221, 49.6842, 52.0635, 44.7690, 37.7183,
-        54.7523, 58.4432, 33.1266, 48.2143, 31.2946, 45.9168, 36.4521, 61.6448,
-        49.4966, 37.1130, 40.1642, 38.9454, 41.6213, 42.1084, 42.4186, 42.5145,
-        34.4180, 33.4178, 32.4243, 63.7925, 50.3810, 51.9030, 46.4714, 35.0566,
-        41.8399, 30.1513, 31.7449, 57.3263, 61.2321, 44.9932, 32.0830, 37.9489,
-        62.4774, 39.2500,
+    let dynamic_power = vec![
+        14.41, 9.38, 14.19, 15.48, 12.15, 16.57, 16.98, 11.02, 17.6, 5.42,
+        13.95, 7.08, 5.66, 18.08, 16.51, 5.86, 9.31, 17.25, 8.48, 7.29,
     ];
+    assert_close!(result.tables[0].columns[2].data, dynamic_power);
 
-    assert_close!(r.tables[1].columns[1].data, data);
+    let execution_time = vec![
+        0.025, 0.019, 0.025, 0.026, 0.022, 0.027, 0.028, 0.021, 0.028, 0.015,
+        0.024, 0.017, 0.015, 0.029, 0.027, 0.015, 0.019, 0.028, 0.018, 0.017,
+    ];
+    assert_close!(result.tables[0].columns[3].data, execution_time);
 }
 
 #[test]
 fn parse_032_640() {
-    let r = tgff::parse(read_fixture("032_640.tgff").as_slice()).unwrap();
+    let result = tgff::parse(read_fixture("032_640.tgff").as_slice()).unwrap();
 
-    assert_eq!(r.graphs.len(), 1);
-    assert_eq!(r.graphs[0].tasks.len(), 640);
-    assert_eq!(r.graphs[0].arcs.len(), 848);
-    assert_eq!(r.graphs[0].deadlines.len(), 259);
+    assert_eq!(result.attributes["HYPERPERIOD".to_string()], 18);
+    assert_eq!(result.graphs.len(), 1);
+    assert_eq!(result.tables.len(), 32);
 
-    assert_eq!(r.tables.len(), 32);
-    for table in r.tables.iter() {
+    let graph = &result.graphs[0];
+    assert_eq!(graph.tasks.len(), 640);
+    assert_eq!(graph.arcs.len(), 848);
+    assert_eq!(graph.deadlines.len(), 259);
+
+    for table in result.tables.iter() {
         assert_eq!(table.attributes.len(), 1);
         assert_eq!(table.columns.len(), 4);
         for column in table.columns.iter() {
@@ -72,7 +71,7 @@ fn parse_032_640() {
 
 fn read_fixture(name: &str) -> String {
     use std::io::fs::PathExtensions;
-    let path = Path::new("fixtures").join(name);
+    let path = Path::new("tests").join_many(["fixtures", name]);
     assert!(path.exists());
     std::io::File::open(&path).read_to_string().unwrap()
 }
