018d460c0b8457914a22912fbbbda44630c5018a
Ivan Ukhov <ivan.ukhov@gmail.com>
2014-11-20 07:43:31 +0100

    Keeping up with the nightly builds

--- tests/lib.rs
+++ tests/lib.rs
@@ -71,7 +71,7 @@ fn parse_032_640() {
 
 fn read_fixture(name: &str) -> String {
     use std::io::fs::PathExtensions;
-    let path = Path::new("tests").join_many(["fixtures", name]);
+    let path = Path::new("tests").join_many(&["fixtures", name]);
     assert!(path.exists());
     std::io::File::open(&path).read_to_string().unwrap()
 }
